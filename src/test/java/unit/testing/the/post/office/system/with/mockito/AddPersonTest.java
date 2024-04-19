48cfef15984cfd81b29f00abb6a1c598b4adfe29
amandaestevez <amandaestevez8@hotmail.com>
2024-04-19 06:21:47 -0300

    04.19.2024 COMMIT 01| ADDS AN EXCEPTION TEST TO THE "AddPersonTest" CLASS

--- src/test/java/unit/testing/the/post/office/system/with/mockito/AddPersonTest.java
+++ src/test/java/unit/testing/the/post/office/system/with/mockito/AddPersonTest.java
@@ -1,6 +1,7 @@
 package unit.testing.the.post.office.system.with.mockito;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,8 +13,10 @@ import simulating.a.post.office.system.LocationData;
 import simulating.a.post.office.system.Person;
 import simulating.a.post.office.system.PostOfficeApi;
 
+import javax.management.RuntimeErrorException;
 import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
 
 @ExtendWith(MockitoExtension.class)
 public class AddPersonTest {
@@ -27,8 +30,8 @@ public class AddPersonTest {
     @Test
     void validateRegistrationData(){
         LocationData locationData = new LocationData("SP", "Santos","St. 232","house", "Whatever");
-        Mockito.when(postOfficeApi.getDataFromZipCode("12345")).thenReturn(locationData);
-        Person person = addPerson.addPerson("Amanda", "12345678", LocalDate.now(), "12345" );
+        Mockito.when(postOfficeApi.getDataFromZipCode(anyString())).thenReturn(locationData);
+        Person person = addPerson.addPerson("Amanda", "12345678", LocalDate.of(1995,1,8), "12345" );
 
         assertEquals("Amanda", person.getName());
         assertEquals("12345678", person.getId());
@@ -37,5 +40,12 @@ public class AddPersonTest {
 
     }
 
+    @Test
+    void addPersonFail() {
+        Mockito.when(postOfficeApi.getDataFromZipCode(anyString())).thenThrow(RuntimeException.class);
+
+        Assertions.assertThrows(RuntimeException.class, () -> addPerson.addPerson("Amanda", "12345678", LocalDate.of(1995, 1, 8), "12345"));
+    }
 }
+
 
