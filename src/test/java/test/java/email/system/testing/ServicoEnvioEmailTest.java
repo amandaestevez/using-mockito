0f651fcc356970cab24023e5c7e15b62c051da39
amandaestevez <amandaestevez8@hotmail.com>
2024-06-25 15:19:00 -0300

    06.24.2024 | Moved Mockito Package

---
+++ src/test/java/test/java/email/system/testing/ServicoEnvioEmailTest.java
@@ -0,0 +1,48 @@
+package test.java.email.system.testing;
+
+
+import main.simulation.Email;
+import main.simulation.Formato;
+import main.simulation.PlataformaDeEnvio;
+import main.simulation.ServicoEnvioEmail;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+
+//Lines 14-15:Creates a public class and calls the @ExtendWith annotation in JUnit
+@ExtendWith(MockitoExtension.class)
+public class ServicoEnvioEmailTest {
+
+    //Lines 19-20: Inserts the instantiated class as a mock
+    //This allows us to test the code without using its dependencies
+    @Mock
+    private PlataformaDeEnvio plataforma;
+
+    //Lines 23-24: Injects the mock PlataformaDeEnvio created by Mockito into the servico field.
+    @InjectMocks
+    private ServicoEnvioEmail servico;
+
+    //Lines 27-28: captures the Email object passed to the emailCaptor method.
+    @Captor
+    private ArgumentCaptor<Email> emailCaptor;
+
+    //Line 31-32: calls the @Test annotation and creates the method to execute the test
+    @Test
+    void validaSeEmailEstaComDadosCorretos() {
+
+        String email = "random@test.com";
+        String mensagem = "Hey there!";
+
+        //Lines 38-39: tests enviaEmail is called on the platform with the correct parameters.
+        servico.enviaEmail(email, mensagem, true);
+        Mockito.verify(plataforma).enviaEmail(emailCaptor.capture());
+
+        //Lines 42-43: Verifies that the service sets the mail format to HTML before sending
+        Email emailCapturado = emailCaptor.getValue();
+        Assertions.assertEquals(Formato.HTML, emailCapturado.getFormato());
+    }
+}
